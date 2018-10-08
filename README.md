ca8ac0e97550dfe2ca13b2e61d41c7cd059ef6aa
Joannes Paanakker <j.paanakker@upcmail.nl>
2018-10-08 16:48:15 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+New website for the "Vereniging Voor Psychiaters in Amsterdam en Omstreken" (VVPAO).
+
+By QED webdevelopment
+main developer: Joannes Paanakker
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
+<h1>Pages#nascholing</h1>
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'informatie', to: 'pages#informatie'
+  get 'nascholing', to: 'pages#nascholing'
+  get 'psychiaters', to: 'pages#psychiaters'
+  get 'lidmaatschap', to: 'pages#lidmaatschap'
+  get 'klachten', to: 'pages#klachten'
+  get 'nieuws', to: 'pages#nieuws'
+  get 'contact', to: 'pages#contact'
 end
+
+
